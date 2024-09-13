ed8571354424cad969a1bb6975fe5e481cb57119
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-13 17:17:20 +0300

    "Updated app.module.ts to import ConfigService from @nestjs/config and modified TypeOrmModule.forRootAsync to use ConfigService for database configuration"

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { UserModule } from './user/user.module'
 import { CategoryModule } from './category/category.module'
 import { AuthModule } from './auth/auth.module'
 import { TransactionModule } from './transaction/transaction.module'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
@@ -17,6 +17,16 @@ import { TypeOrmModule } from '@nestjs/typeorm'
         ConfigModule.forRoot({ isGlobal: true }),
         TypeOrmModule.forRootAsync({
             imports: [ConfigModule],
+            useFactory: (configService: ConfigService) => ({
+                type: 'postgres',
+                host: configService.get('PG_HOST'),
+                port: configService.get('PG_PORT'),
+                username: configService.get('PG_USER'),
+                password: configService.get('PG_PASSWORD'),
+                database: configService.get('PG_DB'),
+                entities: [__dirname + '/**/*.entity{.ts,.js}'],
+                synchronize: true,
+            }),
         }),
     ],
     controllers: [AppController],
