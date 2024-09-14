86f654e155906cacd0b8edbb8276c108a0b97008
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-14 11:12:26 +0300

    "Added TypeORM decorators and properties to Category entity"

--- src/category/entities/category.entity.ts
+++ src/category/entities/category.entity.ts
@@ -1,1 +1,25 @@
-export class Category {}
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+} from 'typeorm'
+
+@Entity()
+export class Category {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    email: string
+
+    @Column()
+    password: string
+
+    @CreateDateColumn()
+    createdAt: Date
+
+    @UpdateDateColumn()
+    updatedAt: Date
+}
