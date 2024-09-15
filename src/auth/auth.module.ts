c86825905e2271de2d4ce9b93b308c0fba0690d9
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-15 12:40:43 +0300

    "Removed auth CRUD methods from AuthService, added UserService dependency and validateUser method"

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { AuthController } from './auth.controller'
+import { UserService } from 'src/user/user.service'
 
 @Module({
+    imports: [UserService],
     controllers: [AuthController],
     providers: [AuthService],
 })
