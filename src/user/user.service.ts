e4c3cda84bba00baa968744b656c9ac8be387841
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-15 11:36:59 +0300

    "Remove UpdateUserDto import and findOne, update, and remove methods from UserService"

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common'
 import { CreateUserDto } from './dto/create-user.dto'
-import { UpdateUserDto } from './dto/update-user.dto'
 
 @Injectable()
 export class UserService {
@@ -10,17 +9,5 @@ export class UserService {
 
     findAll() {
         return `This action returns all user`
-    }
-
-    findOne(id: number) {
-        return `This action returns a #${id} user`
-    }
-
-    update(id: number, updateUserDto: UpdateUserDto) {
-        return `This action updates a #${id} user`
-    }
-
-    remove(id: number) {
-        return `This action removes a #${id} user`
     }
 }
