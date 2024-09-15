0b9a8096ac329cf8179ff32df57b5dac3f0b7112
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-15 12:48:29 +0300

    "Updated auth module to import UserModule instead of UserService"

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { AuthController } from './auth.controller'
-import { UserService } from 'src/user/user.service'
+import { UserModule } from 'src/user/user.module'
 
 @Module({
-    imports: [UserService],
+    imports: [UserModule],
     controllers: [AuthController],
     providers: [AuthService],
 })
