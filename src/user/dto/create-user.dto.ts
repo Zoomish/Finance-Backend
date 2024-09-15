7cf5c45f5c8a61c963e2323180210117c5f9c510
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-15 11:32:13 +0300

    "Added class-validator decorators to CreateUserDto"

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,11 @@
+import { IsEmail, IsNotEmpty, MaxLength, MinLength } from 'class-validator'
+
 export class CreateUserDto {
+    @IsEmail()
     email: string
+
+    @IsNotEmpty()
+    @MinLength(8)
+    @MaxLength(20)
     password: string
 }
