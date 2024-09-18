2efc46b6394678d0d2f636b97cbb4b9bb70ba758
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-18 18:39:51 +0300

    "Added properties to CreateTransactionDto class"

--- src/transaction/dto/create-transaction.dto.ts
+++ src/transaction/dto/create-transaction.dto.ts
@@ -1,1 +1,8 @@
-export class CreateTransactionDto {}
+import { Category } from 'src/category/entities/category.entity'
+
+export class CreateTransactionDto {
+    title: string
+    amount: number
+    type: 'expense' | 'income'
+    category: Category
+}
