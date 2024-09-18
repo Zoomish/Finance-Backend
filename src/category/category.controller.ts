08a7ea3df2a8c7a114a53126baf1f9fc3b972e3f
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-18 17:00:18 +0300

    "Added JwtAuthGuard to CategoryController's create method"

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -7,16 +7,19 @@ import {
     Param,
     Delete,
     Req,
+    UseGuards,
 } from '@nestjs/common'
 import { CategoryService } from './category.service'
 import { CreateCategoryDto } from './dto/create-category.dto'
 import { UpdateCategoryDto } from './dto/update-category.dto'
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard'
 
 @Controller('category')
 export class CategoryController {
     constructor(private readonly categoryService: CategoryService) {}
 
     @Post()
+    @UseGuards(JwtAuthGuard)
     create(@Body() createCategoryDto: CreateCategoryDto, @Req() req) {
         return this.categoryService.create(createCategoryDto, +req.user.id)
     }
