c80e376fb94580b4ace997728ef6c9e3e0609a80
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-18 16:32:02 +0300

    "Swap order of @UseGuards and route decorators in AuthController"

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,14 +7,14 @@ import { JwtAuthGuard } from './guards/jwt-auth.guard'
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
-    @UseGuards(LocalAuthGuard)
     @Post('login')
+    @UseGuards(LocalAuthGuard)
     async login(@Request() req) {
         return this.authService.login(req.user)
     }
 
-    @UseGuards(JwtAuthGuard)
     @Get('profile')
+    @UseGuards(JwtAuthGuard)
     getProfile(@Request() req) {
         return req.user
     }
