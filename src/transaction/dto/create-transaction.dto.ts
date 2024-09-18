8077a60ab033f5f4b10d041ba74db3ca2e576ff5
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-18 18:40:12 +0300

    int

--- src/transaction/dto/create-transaction.dto.ts
+++ src/transaction/dto/create-transaction.dto.ts
@@ -1,8 +1,10 @@
 import { Category } from 'src/category/entities/category.entity'
+import { User } from 'src/user/entities/user.entity'
 
 export class CreateTransactionDto {
     title: string
     amount: number
     type: 'expense' | 'income'
     category: Category
+    user: User
 }
