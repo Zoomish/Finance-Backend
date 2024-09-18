e33a954890ce86ba94bb17395459ea6f457749c3
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-18 16:46:55 +0300

    "Added class-validator annotations and user property to CreateCategoryDto"

--- src/category/dto/create-category.dto.ts
+++ src/category/dto/create-category.dto.ts
@@ -1,1 +1,10 @@
-export class CreateCategoryDto {}
+import { IsNotEmpty, IsOptional } from 'class-validator'
+import { User } from 'src/user/entities/user.entity'
+
+export class CreateCategoryDto {
+    @IsNotEmpty()
+    titile: string
+
+    @IsOptional()
+    user?: User
+}
