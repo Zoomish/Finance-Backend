47c03cdadae2922dbc212ebb8f9dfb9b1b663c46
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-19 19:34:00 +0300

    "Added type parameter check in AuthorGuard"

--- src/guard/author.guard.ts
+++ src/guard/author.guard.ts
@@ -3,6 +3,14 @@ import { CanActivate, ExecutionContext } from '@nestjs/common'
 export class AuthorGuard implements CanActivate {
     async canActivate(context: ExecutionContext): Promise<boolean> {
         const req = context.switchToHttp().getRequest()
+        const { id, type } = req.params
+
+        switch (type) {
+            case 'transactions':
+            case 'category':
+            default:
+                throw new Error('Wrong type')
+        }
         return true
     }
 }
