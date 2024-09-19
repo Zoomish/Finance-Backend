15a8ca5193625969ced08f9ce0bd9fa16dec51fb
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-19 19:59:15 +0300

    "Added CategoryService and TransactionService to AuthorGuard constructor"

--- src/guard/author.guard.ts
+++ src/guard/author.guard.ts
@@ -1,6 +1,13 @@
 import { CanActivate, ExecutionContext } from '@nestjs/common'
+import { CategoryService } from 'src/category/category.service'
+import { TransactionService } from 'src/transaction/transaction.service'
 
 export class AuthorGuard implements CanActivate {
+    constructor(
+        private readonly transactionService: TransactionService,
+        private readonly categoryService: CategoryService
+    ) {}
+
     async canActivate(context: ExecutionContext): Promise<boolean> {
         const req = context.switchToHttp().getRequest()
         const { id, type } = req.params
