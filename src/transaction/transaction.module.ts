cfe351633b2947a349b4610f0d21c0a1c40ad2b9
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-19 20:17:09 +0300

    "Added Category entity to TypeOrmModule imports in TransactionModule"

--- src/transaction/transaction.module.ts
+++ src/transaction/transaction.module.ts
@@ -3,9 +3,10 @@ import { TransactionService } from './transaction.service'
 import { TransactionController } from './transaction.controller'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Transaction } from './entities/transaction.entity'
+import { Category } from 'src/category/entities/category.entity'
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Transaction])],
+    imports: [TypeOrmModule.forFeature([Transaction, Category])],
     controllers: [TransactionController],
     providers: [TransactionService],
 })
