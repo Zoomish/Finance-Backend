9ca111ca9fdc873ec82440d99678a44dd174ef84
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-19 19:32:29 +0300

    Added a line to retrieve the HTTP request in the AuthorGuard's canActivate method.

--- src/guard/author.guard.ts
+++ src/guard/author.guard.ts
@@ -2,6 +2,7 @@ import { CanActivate, ExecutionContext } from '@nestjs/common'
 
 export class AuthorGuard implements CanActivate {
     async canActivate(context: ExecutionContext): Promise<boolean> {
+        const req = context.switchToHttp().getRequest()
         return true
     }
 }
